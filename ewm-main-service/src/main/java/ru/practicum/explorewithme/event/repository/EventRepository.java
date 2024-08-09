cb16a522390c7b72a55479c0d3d654713a4ed437
gingi <ginginivan@gmail.com>
2024-08-10 03:25:39 +0700

    На проверку

--- ewm-main-service/src/main/java/ru/practicum/explorewithme/event/repository/EventRepository.java
+++ ewm-main-service/src/main/java/ru/practicum/explorewithme/event/repository/EventRepository.java
@@ -14,20 +14,18 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findByInitiatorId(Long initiatorId, Pageable pageable);
 
     Optional<Event> findByIdAndInitiatorId(Long eventId, Long initiatorId);
-
     @Query("SELECT e FROM Event e " +
             "WHERE (:users IS NULL OR e.initiator.id IN :users) " +
             "AND (:states IS NULL OR e.state IN :states) " +
             "AND (:categories IS NULL OR e.category.id IN :categories) " +
             "AND (CAST(:rangeStart AS timestamp) IS NULL OR e.eventDate >= :rangeStart) " +
-            "AND (CAST(:rangeEnd AS timestamp) IS NULL OR e.eventDate <= :rangeEnd) " +
-            "AND e.state = 'PUBLISHED'")
+            "AND (CAST(:rangeEnd AS timestamp) IS NULL OR e.eventDate <= :rangeEnd)")
     List<Event> searchEvents(List<Long> users,
-                             List<State> states,
-                             List<Long> categories,
-                             LocalDateTime rangeStart,
-                             LocalDateTime rangeEnd,
-                             Pageable pageable);
+                                  List<State> states,
+                                  List<Long> categories,
+                                  LocalDateTime rangeStart,
+                                  LocalDateTime rangeEnd,
+                                  Pageable pageable);
 
     @Query("SELECT e FROM Event e " +
             "WHERE (:text IS NULL OR LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) " +
