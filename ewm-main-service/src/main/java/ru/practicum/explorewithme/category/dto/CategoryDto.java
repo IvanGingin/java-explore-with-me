a58b14abbdbf9a39237ea461fe9f18b32e45e1bf
gingi <ginginivan@gmail.com>
2024-08-17 21:24:11 +0700

    изменения

--- ewm-main-service/src/main/java/ru/practicum/explorewithme/category/dto/CategoryDto.java
+++ ewm-main-service/src/main/java/ru/practicum/explorewithme/category/dto/CategoryDto.java
@@ -1,6 +1,8 @@
 package ru.practicum.explorewithme.category.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.*;
 
